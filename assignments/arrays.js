c5c390768cb3f05f73cebfdcc54159daabe72a08
Apiyo4 <odingoannette@gmail.com>
2019-10-14 16:54:39 +0300

    Finish function-conversion.js MVP

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -118,7 +118,6 @@ for(let i=0; i<inventory.length; i++){
   if((inventory[i].car_make == "BMW") || (inventory[i].car_make == "Audi")){
     BMWAndAudi.push(inventory[i]);
   }
-  
 }
 const ans = JSON.stringify(BMWAndAudi);
 console.log(ans);
